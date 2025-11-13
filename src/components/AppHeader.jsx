48ae5765bd969c502c8845668458a7c5c5071771
Filmmie0-0 <rattapum.lim@spumail.net>
2025-11-14 00:57:58 +0700

    delete-source

--- src/components/AppHeader.jsx
+++
@@ -1,9 +0,0 @@
-const AppHeader = () => {
-    return (
-        <div className="text-center"style={{marginTop: "5px",height: "150px",backgroundImage: "url('./image/sword.jpg')", backgroundSize: "cover",backgroundPosition: "center",}}>
-            <h1 className="text-white">CSI205 การพัฒนาโปรแกรมส่วนหน้า</h1>
-        </div>
-    );
-}
-
-export default AppHeader;
